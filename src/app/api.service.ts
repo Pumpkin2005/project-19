e5b7c5f9b7778269631ad5d98de597ff31d2a425
Maria <mariia.harbuzenko.pm.2022@lpnu.ua>
2025-04-01 23:56:41 +0300

    5 commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,12 +11,11 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<any> {
-    return this.http.get(this.apiUrl);
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
   }
 
-  postUser(data: any): Observable<any> {
-    return this.http.post(this.apiUrl, data);
+  postUser(user: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user);
   }
-
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,8 +2,15 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../api.service';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+interface User {
+  email: string;
+  password: string;
+  nickname?: string;
+}
 @Component({
   selector: 'app-registration',
   standalone: true,
@@ -12,67 +19,82 @@ import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
-export class RegistrationComponent implements OnInit{
-  user = {
-    nickname: '',
-    email: '',
-  };
-  fb = inject(FormBuilder);
-  registrationForm !: FormGroup;
+export class RegistrationComponent implements OnInit {
+  registrationForm!: FormGroup;
   isRegistration: boolean = true;
-  users: any[] = [];
+  loggedIn: boolean = false;
+  users: User[] = [];
+  user: User = { email: '', password: '', nickname: '' };
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
 
   constructor(private apiService: ApiService) {}
-  public Registration: any[] = [
-    {
-      password: "Password",
-      email: "Email"
 
-    },
-  ];
-  public Regist: string[] = [
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      nickname: [''],
+    });
 
-  ];
+    this.loadUsers();
+  }
+
   toggleLoginView() {
     this.isRegistration = !this.isRegistration;
   }
-  ngOnInit(): void {
-    this.registrationForm=this.fb.group({
-      email:['',Validators.compose([Validators.required, Validators.email])],
-      password:['',Validators.required],
-      nickname:['',Validators.required]
-    })
+
+  loadUsers() {
     this.apiService.getUsers().subscribe({
-      next: (data) => {
-        this.users = data;
+      next: (data) => this.users = data,
+      error: (err) => console.error('Помилка отримання користувачів', err),
+    });
+  }
+
+  // Реєстрація користувача
+  registerUser() {
+    if (this.registrationForm.invalid) {
+      alert('Будь ласка, заповніть всі поля правильно.');
+      return;
+    }
+
+    const newUser = {
+      email: this.registrationForm.value.email,
+      password: this.registrationForm.value.password,
+      nickname: this.registrationForm.value.nickname
+    };
+
+    console.log('Відправка нового користувача:', newUser); // Дебаг
+
+    this.apiService.postUser(newUser).subscribe({
+      next: (response) => {
+        console.log('Успішна відповідь від сервера:', response);
+        alert('Користувач успішно зареєстрований!');
+        this.users.push(response);
+        this.registrationForm.reset();
+        this.isRegistration = true;
       },
       error: (err) => {
-        console.error('Error when receiving data', err);
+        console.error('Помилка реєстрації:', err);
+        alert('Помилка реєстрації, спробуйте ще раз!');
       },
     });
   }
-  registerUser() {
-    this.apiService.postUser(this.user).subscribe({
-      next: (response) => {
-        alert('User successfully registered!');
-        console.log(response);
-      },
-      error: (error) => {
-        alert('Registration error!');
-        console.error(error);
-      },
-    });
-  }
-  reset() {
-    // Reset errors for all form controls
-    this.registrationForm.controls['email'].setErrors(null);
-    this.registrationForm.controls['password'].setErrors(null);
-    this.registrationForm.controls['nickname'].setErrors(null);
-  }
-  login()
-  {
-    console.log(this.registrationForm.value);
-  }
-  data: any;
 
+  login() {
+    const { email, password } = this.registrationForm.value;
+
+    const foundUser = this.users.find((user: User) =>
+      user.email === email && user.password === password
+    );
+
+    if (foundUser) {
+      alert(`Ласкаво просимо, ${foundUser.nickname || 'користувач'}!`);
+      this.loggedIn = true;
+      this.router.navigate(['/home']);
+    } else {
+      alert('Невірний email або пароль!');
+    }
+  }
 }
